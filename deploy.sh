d76008d4596c100910a78868fafa6b55238150b0
Daniil <temnay2@icloud.com>
2025-02-20 00:46:17 +0300

    update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -8,13 +8,13 @@ echo "Pulling latest changes..."
 git pull
 
 echo "Pulling updated Docker images..."
-docker-compose pull
+docker compose pull
 
 echo "Stopping and removing existing containers..."
-docker-compose down || true
+docker compose down || true
 
 echo "Starting updated services..."
-docker-compose up -d
+docker compose up -d
 
 echo "Removing unused Docker images..."
 docker image prune -f
